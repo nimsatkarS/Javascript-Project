48d0662107d2a2471d6e104d4788419b806775a5
Sanket_Nimsatkar <121743873+nimsatkarS@users.noreply.github.com>
2023-05-22 05:53:49 +0530

    This is the Accordion using js

---
+++ JS Project/Question.js
@@ -0,0 +1,31 @@
+/*const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn){
+btns.addEventListener("click",function (e) {
+const question = e.currentTarget.parentElement;
+parrentElement;
+question.classList.toggle("show-text");
+
+});
+}); */
+
+/* here the second step*/
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
